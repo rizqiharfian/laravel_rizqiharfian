5bd569e4122465d6ad54acd3feb2ff255d2f5b88
rizqiharfian <jayarayak99@gmail.com>
2025-09-23 01:04:29 +0700

    update json

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: 'public/build',
+    manifest: true,
+    rollupOptions: {
+      input: ['resources/js/app.js', 'resources/css/app.css'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'resources/js'),
+    },
+  },
 });
